7ccfad36d01e064e9199dee2f2305eebb7c3dd49
JonNazrisho <jnazrisho@gmail.com>
2025-01-02 08:52:50 -0500

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -6,9 +6,10 @@ class Obstacles():
     def __str__(self):
         return f"{self.cactus}, {self.quicksand}"
     def do_damage(self, player):
-        player.health = player.health - self.damage
+        player['health'] -= self.damage
         print(f'Player health is now {player.health}')
     def cactus_damage():
-        cactus.do_damage(player)
+        self.do_damage(player)
         
+obstacle = Obstacles("cactus", "quicksand", 10)
 player = {'health':100}
