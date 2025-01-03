fefcb205c7b32dc4ff6423e5acda420d85e532ba
JonNazrisho <jnazrisho@gmail.com>
2025-01-03 09:04:17 -0500

    gf

--- obstacles.py
+++ obstacles.py
@@ -3,4 +3,23 @@ class Obstacles():
         self.quicksand = quicksand
         self.cactus = cactus
     def __str__(self):
+<<<<<<< Updated upstream
         return f"{self.quicksand}, {self.cactus}" 
+=======
+        return f"{self.cactus}, {self.quicksand}"
+    def do_damage(self, player):
+        player['health'] -= self.damage
+        if player['health'] < 0: 
+            player['health'] = 0
+        print(f'Player health is now {player['health']}')
+    def cactus_damage(self, player):
+        self.do_damage(player)
+    def quicksand_damage(self, player):
+        self.do_damage(player)
+        
+obstacle = Obstacles("cactus", "quicksand", 10)
+player = {'health':100}
+
+obstacle.cactus_damage(player)
+obstacle.quicksand_damage(player)
+>>>>>>> Stashed changes
